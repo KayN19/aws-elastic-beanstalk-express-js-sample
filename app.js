cd714ab2d0c8ef83e305fc1339c693bfe5a2e004
KayN19 <nwankwo.ok@yahoo.com>
2023-12-05 21:59:59 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello App!'));
+app.get('/', (req, res) => res.send('Hello App!, 5 stars'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
